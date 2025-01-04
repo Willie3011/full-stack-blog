ffa05dd8be0871e10bf49a85db38b30d71030d86
Willem April <sirapril121@gmail.com>
2025-01-04 09:36:39 +0200

    Created route files for user, post and comment

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
-import express from 'express'
+import express from "express";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
 
-const app = express()
+const app = express();
 
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
 
 app.listen(3000, () => {
-    console.log("server is running")
-})
+  console.log("server is running");
+});
