1d678a7f7b23b435cd8cf7f0b463f779139cd4af
Willem April <sirapril121@gmail.com>
2025-01-04 22:08:34 +0200

    Protected the post routes using clerk express

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,24 +1,51 @@
-import Post from '../models/post.model.js'
+import Post from "../models/post.model.js";
+import User from "../models/user.model.js";
 
 export const getPosts = async (req, res) => {
-    const posts = await Post.find()
-    res.status(200).send(posts)
-}
+  const posts = await Post.find();
+  res.status(200).send(posts);
+};
 
 export const getPost = async (req, res) => {
-    const post = await Post.findOne({slug:req.params.slug})
-    res.status(200).send(post)
-}
+  const post = await Post.findOne({ slug: req.params.slug });
+  res.status(200).send(post);
+};
 
 export const createPost = async (req, res) => {
-    const newPost = new Post(req.body)
+  const clerkUserId = req.auth.userId;
 
-    const post = await newPost.save()
-    res.status(200).send(post)
-}
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated!");
+  }
+
+  const user = await User.findOne({ clerkUserId });
+
+  if (!user) {
+    return res.status(404).json("User not found!");
+  }
+
+  const newPost = new Post({ user: user._id, ...req.body });
+
+  const post = await newPost.save();
+  res.status(200).send(post);
+};
 
 export const deletePost = async (req, res) => {
-    const post = await Post.findByIdAndDelete(req.params.id);
+  const clerkUserId = req.auth.userId;
 
-    res.status(200).json("This post have been deleted")
-}
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated!");
+  }
+
+  const user = await User.findOne({ clerkUserId });
+  const deletedPost = await Post.findByIdAndDelete({
+    _id: req.params.id,
+    user: user._id,
+  });
+
+  if(!deletedPost){
+    return res.status(403).json("You can only delete your posts!")
+  }
+
+  res.status(200).json("This post have been deleted");
+};
--- backend/index.js
+++ backend/index.js
@@ -4,24 +4,41 @@ import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 import webHookRouter from "./routes/webhook.route.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
+app.use(clerkMiddleware());
 app.use("/webhooks", webHookRouter);
-app.use(express.json())
+app.use(express.json());
+
+// app.get("/auth-state", (req, res) => {
+//   const authState = req.auth;
+//   res.json(authState);
+// })
+
+app.get("/protect", (req, res) => {
+  const {userId} = req.auth;
+  
+  if(!userId) {
+    return res.status(401).json("not authenticated!")
+  }
+
+  res.status(200).json("content")
+})
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 
-app.use((error, req, res, next)=>{
+app.use((error, req, res, next) => {
   res.status(error.status || 500);
 
   res.json({
     message: error.message || "Something went wrong",
     status: error.status,
-    stack:error.stack,
-  })
-})
+    stack: error.stack,
+  });
+});
 
 app.listen(3000, () => {
   connectDB();
