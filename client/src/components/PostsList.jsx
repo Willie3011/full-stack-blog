fd9fe220c3e1ce9f1c60c397f6dc95c17734214b
Willem April <sirapril121@gmail.com>
2025-01-05 10:42:02 +0200

    Updated write page to send data to the backend using react query and axios

--- client/src/components/PostsList.jsx
+++ client/src/components/PostsList.jsx
@@ -1,6 +1,23 @@
+import { useQuery } from "@tanstack/react-query"
 import PostListItem from "./PostListItem"
+import axios from 'axios'
+
+const fetchPosts = async () => {
+  const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts`)
+  return res.data;
+}
 
 const PostsList = () => {
+  const {isPending, error, data} = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () => fetchPosts()
+  })
+
+  if(isPending) return 'Loading....'
+
+  if(error) return 'An error has occured' + error.message;
+
+  console.log(data)
   return (
     <div className="flex flex-col gap-12 mb-8">
       <PostListItem/>
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,9 +1,34 @@
-import { useUser } from "@clerk/clerk-react";
+import { useAuth, useUser } from "@clerk/clerk-react";
 import "react-quill-new/dist/quill.snow.css";
 import ReactQuill from "react-quill-new";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Write = () => {
   const { isLoaded, isSignedIn } = useUser();
+  const [value, setValue] = useState("");
+
+  const navigate = useNavigate();
+
+  const { getToken } = useAuth();
+
+  const mutation = useMutation({
+    mutationFn: async (newPost) => {
+      const token = await getToken();
+      return axios.post(`${import.meta.env.VITE_API_URL}/posts`, newPost, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    onSuccess: (res) => {
+      toast.success("Post has been created");
+      navigate(`/${res.data.slug}`);
+    },
+  });
 
   if (isLoaded && !isSignedIn) {
     return <div className="">You should login!</div>;
@@ -13,15 +38,43 @@ const Write = () => {
     return <div className="">Loading...</div>;
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const data = {
+      title: formData.get("title"),
+      category: formData.get("category"),
+      desc: formData.get("desc"),
+      content: value,
+    };
+
+    console.log(data);
+    mutation.mutate(data);
+  };
+
   return (
     <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6">
       <h1 className="text-xl font-light">Create a New Post</h1>
-      <form className="flex flex-col gap-6 flex-1 mb-6">
-        <button className="p-2 w-max shadow-md rounded-xl text-sm text-gray-500 bg-white">Add a cover image</button>
-        <input type="text" className="text-4xl font-semibold bg-transparent outline-none" placeholder="My Awesome Story" />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6 flex-1 mb-6">
+        <button className="p-2 w-max shadow-md rounded-xl text-sm text-gray-500 bg-white">
+          Add a cover image
+        </button>
+        <input
+          type="text"
+          className="text-4xl font-semibold bg-transparent outline-none"
+          placeholder="My Awesome Story"
+          name="title"
+        />
         <div className="flex items-center gap-4">
-          <label htmlFor="" className="text-sm">Choose a category</label>
-          <select name="cat" id="" className="p-2 rounded-xl bg-white outline-none shadow-md">
+          <label htmlFor="" className="text-sm">
+            Choose a category
+          </label>
+          <select
+            name="category"
+            id=""
+            className="p-2 rounded-xl bg-white outline-none shadow-md">
             <option value="general">General</option>
             <option value="web-design">Web Design</option>
             <option value="development">Development</option>
@@ -30,9 +83,23 @@ const Write = () => {
             <option value="marketing">Marketing</option>
           </select>
         </div>
-        <textarea name="desc" className="p-4 rounded-xl bg-white outline-none shadow-md" placeholder="A short Description" />
-        <ReactQuill theme="snow" className="flex-1 rounded-xl bg-white shadow-md"/>
-        <button className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36">Send</button>
+        <textarea
+          name="desc"
+          className="p-4 rounded-xl bg-white outline-none shadow-md"
+          placeholder="A short Description"
+        />
+        <ReactQuill
+          theme="snow"
+          className="flex-1 rounded-xl bg-white shadow-md"
+          value={value}
+          onChange={setValue}
+        />
+        <button
+          disabled={mutation.isPending}
+          className="bg-blue-800 disabled:bg-blue-400 disabled:cursor-not-allowed text-white font-medium rounded-xl mt-4 p-2 w-36">
+          {mutation.isPending ? "Loading..." : "Send"}
+        </button>
+        {mutation.isError && <span>{mutation.error.message}</span>}
       </form>
     </div>
   );
