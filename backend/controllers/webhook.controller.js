611480fd44ae17f51e553f23b36c470f692326ac
Willem April <sirapril121@gmail.com>
2025-01-07 13:29:55 +0200

    Fixed an error on the clerk webhook

--- backend/controllers/webhook.controller.js
+++ backend/controllers/webhook.controller.js
@@ -16,22 +16,59 @@ export const clerkWebHook = async (req, res) => {
   try {
     evt = wh.verify(payload, headers);
   } catch (error) {
-    res.status(400).json({
+    console.log("Something went wrong! " + error);
+    return res.status(400).json({
       message: "Webhook verification failed!",
     });
   }
 
   if (evt.type === "user.created") {
-    const newUser = new User({
-      clerkUserId: evt.data.id,
-      username: evt.data.username || evt.data.email_addresses[0].email_address,
-      email: evt.data.email_addresses[0].email_address,
-      img: evt.data.profile_image_url,
-    });
+    const clerkId = evt.data.id;
 
-    await newUser.save()
+    //check if cler id is available
+    if (!clerkId) {
+      console.error("Missing Clerk ID in event data.");
+      return res
+        .status(400)
+        .json({ message: "Invalid Clerk user data: Clerk ID is null." });
+    }
+
+    try {
+      //check if there's already a user with the same clerkUserId
+      const existingUser = await User.findOne({ clerkUserId: clerkId });
+      if (existingUser) {
+        console.log("User already exists:", existingUser);
+        return res.status(200).json({ message: "User already exists." });
+      }
+
+      //create a new user
+      const newUser = new User({
+        clerkUserId: clerkId,
+        username:
+          evt.data.username ||
+          evt.data.email_addresses[0]?.email_address ||
+          "Unknown",
+        email:
+          evt.data.email_addresses[0]?.email_address || "No email provided",
+        img: evt.data.profile_image_url || "",
+      });
+
+      //save user
+      const user = await newUser.save();
+      console.log("New user saved:", user);
+
+      return res
+        .status(200)
+        .json({ message: "User created and saved to the database" });
+    } catch (error) {
+      console.error("Error saving new user:", error);
+      
+      return res.status(500).json({
+        message: "Error saving user to the database",
+      });
+    }
   }
   return res.status(200).json({
     message: "Webhook received",
-  })
+  });
 };
