6c06f3d11b2e8a60d0616a7463aa2bb4e9e2554d
Willem April <sirapril121@gmail.com>
2025-01-08 11:41:45 +0200

    Created routes to save posts and read saved posts

--- client/src/components/PostMenuActions.jsx
+++ client/src/components/PostMenuActions.jsx
@@ -1,17 +1,125 @@
-import { FaBookmark, FaRegTrashCan } from "react-icons/fa6";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { FaBookmark, FaRegTrashCan, FaRegBookmark } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const PostMenuActions = () => {
+const PostMenuActions = ({ post }) => {
+  const { user } = useUser();
+  const { getToken } = useAuth();
+
+  const navigate = useNavigate();
+
+  const queryClient = useQueryClient();
+
+  const {
+    isPending,
+    error,
+    data: savedPosts,
+  } = useQuery({
+    queryKey: ["savedPosts"],
+    queryFn: async () => {
+      const token = await getToken();
+      return axios.get(`${import.meta.env.VITE_API_URL}/users/saved`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+  });
+
+  const isAdmin = user?.publicMetadata?.role === "admin" || false
+  const isSaved = savedPosts?.data?.some((p) => p === post._id) || false;
+
+  const saveMutation = useMutation({
+    mutationFn: async () => {
+      const token = await getToken();
+      return axios.patch(
+        `${import.meta.env.VITE_API_URL}/users/save`,
+        { postId: post._id },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["savedPosts"] });
+    },
+    onError: (err) => {
+      toast.error(err.response.data);
+    },
+  });
+
+  const deleteMutation = useMutation({
+    mutationFn: async () => {
+      const token = await getToken();
+      return axios.delete(`${import.meta.env.VITE_API_URL}/posts/${post._id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    onSuccess: () => {
+      toast.success("Post deleted successfully!");
+      navigate("/");
+    },
+    onError: (err) => {
+      toast.error(err.response.data);
+    },
+  });
+
+  const handleDelete = () => {
+    deleteMutation.mutate();
+  };
+
+  const handleSave = () => {
+    if (!user) {
+      return navigate("/login");
+    }
+    saveMutation.mutate();
+  };
   return (
     <div>
       <h1 className="mt-8 mb-4 text-sm font-medium">Actions</h1>
-      <div className="flex items-center gap-2 py-2 text-sm cursor-pointer">
-        <FaBookmark className="text-base"/>
-        <span>Save this post</span>
-      </div>
-      <div className="flex items-center gap-2 py-2 text-sm cursor-pointer">
-        <FaRegTrashCan className="text-red-500 text-base" />
-        <span>Save this post</span>
-      </div>
+      {isPending ? (
+        "Loading..."
+      ) : error ? (
+        "Saved posts fetching failed!"
+      ) : (
+        <div
+          className="flex items-center gap-2 py-2 text-sm cursor-pointer"
+          onClick={handleSave}>
+          {saveMutation.isPending ? (
+            isSaved ? (
+              <FaRegBookmark />
+            ) : (
+              <FaBookmark />
+            )
+          ) : isSaved ? (
+            <FaBookmark className="text-base" />
+          ) : (
+            <FaRegBookmark />
+          )}
+          <span>Save this post</span>
+          {saveMutation.isPending && (
+            <span className="text-xs">(in progress)</span>
+          )}
+        </div>
+      )}
+      {user && (post.user.username === user.username || isAdmin) && (
+        <div
+          className="flex items-center gap-2 py-2 text-sm cursor-pointer"
+          onClick={handleDelete}>
+          <FaRegTrashCan className="text-red-500 text-base" />
+          <span>Delete this post</span>
+          {deleteMutation.isPending && (
+            <span className="text-xs">(in progress)</span>
+          )}
+        </div>
+      )}
     </div>
   );
 };
