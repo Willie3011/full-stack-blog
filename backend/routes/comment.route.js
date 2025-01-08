77166cd57a9e06f89c10213fc07b398296afe3ca
Willem April <sirapril121@gmail.com>
2025-01-08 09:09:47 +0200

    Created comment routes and CRUD methods

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,5 +1,10 @@
+import { getPostComments, addComment, deleteComment } from "../controllers/comment.controller.js";
 import express from "express";
 
 const router = express.Router();
+
+router.get("/:postId", getPostComments);
+router.post("/:postId", addComment);
+router.delete("/:id", deleteComment);
 
 export default router;
